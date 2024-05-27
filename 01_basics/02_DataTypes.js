35be80ce616179d60ac513db13295fd34b2e60dc
Rishabh Lawaniya <114868059+RishabhLawaniya@users.noreply.github.com>
2024-05-27 11:24:20 +0000

    Datatypes done

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,32 @@
+("use strict") // treat all js code as newer version/JS
+
+alert( 3 + 3 ) // we are using node js not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Rishabh"); // semi colon not needed    
+
+// JS standards - ECMA Standards - tc:39.es, MDN(Not standard)
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53(not sure)
+// bigint
+// string => ""( single or double quotes both accepted)
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
